d8f43ab25a9b5083cdbd84fb54d9a8753721a439
Davey Wright <Davey Wright davey.w.wright@gmail.com>
2018-01-16 15:25:38 +0000

    update schema remove 2 columns

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180111143059) do
+ActiveRecord::Schema.define(version: 20180116151238) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,10 +23,8 @@ ActiveRecord::Schema.define(version: 20180111143059) do
     t.float    "distance"
     t.text     "description"
     t.text     "directions"
-    t.boolean  "free_parking"
-    t.text     "where_to_park"
-    t.datetime "created_at",    null: false
-    t.datetime "updated_at",    null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
 end
