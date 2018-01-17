59a6e329f7a7a00d0726926f8d1914cff86c1d33
Davey Wright <Davey Wright davey.w.wright@gmail.com>
2018-01-17 16:10:07 +0000

    update places controll to handle form submission

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -9,12 +9,18 @@ class PlacesController < ApplicationController
 	end
 
 	def create
-		
+		params[:place][:time] = params[:place][:hours] + ":" + params[:place][:minutes]
+		Place.create( place_params )
+		redirect_to root_path
+	end
+
+private
+	
+	def place_params
+		params.require(:place).permit(:title, :difficulty, :time, :distance, :description, :directions, {terrain: []} )
 	end
 
 end
-
-# binding.pry
 
 # [1, 2, 3].map { |n|  n.to_s }
 # [1, 2, 3].map(&:to_s)
--- app/models/place.rb
+++ app/models/place.rb
@@ -1,10 +1,7 @@
 class Place < ApplicationRecord
-	
+
+	def self.terrain_options
+		['Sand', 'Grass', 'Rocks', 'Marsh', 'Hilly']
+	end
 
 end
-
-
-
-# def self.terrain_options
-#		['Sand', 'Grass', 'Rocks', 'Marsh', 'Hilly']
-#	end
