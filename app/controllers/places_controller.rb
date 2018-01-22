8d6e95fe7c0e769a8d4522c8f6962df8e97f62f7
Davey Wright <Davey Wright davey.w.wright@gmail.com>
2018-01-22 14:49:55 +0000

    Add form validations

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -3,23 +3,40 @@ class PlacesController < ApplicationController
 	before_action :authenticate_user!, only: [ :new, :create, :edit, :update, :destroy ]
 
 	def index
-		@places = Place.all.page params[:page]
+		@place = Place.all.page params[:page]
 	end
 
+
 	def new
-		@places = Place.new
+		@place = Place.new
 	end
 
 
 	def create
-		params[:place][:time] = params[:place][:hours] + ":" + params[:place][:minutes]
-		current_user.places.create( place_params )
-		redirect_to root_path
+
+		h = params[:place][:hours]
+		m = params[:place][:minutes]
+
+		if h.present? || m.present?
+			params[:place][:time] = h + ":" + m
+		end
+
+		@place = current_user.places.create( place_params )
+
+		binding.pry
+
+		if @place.valid?
+			redirect_to root_path	
+		else
+			render :new, status: :unprocessable_entity
+		end
 	end
+
 
 	def show
 		@place = Place.find( params[:id] )
 	end
+
 
 	def edit
 		@place = Place.find( params[:id] )
@@ -27,7 +44,9 @@ class PlacesController < ApplicationController
 		if @place.user != current_user
 			return render text: 'Access Denied!', status: :forbidden
 		end
+
 	end
+
 
 	def update
 		@place = Place.find( params[:id] )
@@ -37,8 +56,15 @@ class PlacesController < ApplicationController
 		end
 
 		@place.update_attributes( place_params )
-		redirect_to root_path
+	
+		if @place.valid?
+			redirect_to root_path
+		else
+			render :edit, status: :unprocessable_entity
+		end
+		
 	end
+
 
 	def destroy
 		@place = Place.find( params[:id] )
@@ -50,8 +76,10 @@ class PlacesController < ApplicationController
 		redirect_to root_path
 	end
 
+
 private
 	
+
 	def place_params
 		params.require(:place).permit(:title, :difficulty, :time, :distance, :description, :directions, {terrain: []} )
 	end
--- app/models/place.rb
+++ app/models/place.rb
@@ -2,8 +2,13 @@ class Place < ApplicationRecord
 
 	def self.terrain_options
 		['Sand', 'Grass', 'Rocks', 'Marsh', 'Hilly']
-
 	end
+
+	validates :title, presence: { message: 'Please enter a title for your route.' }
+	validates :difficulty, presence: { message: 'Please choose a level of difficulty.' }
+	validates :time, presence: { message: 'Please enter the time it takes to walk this route.' }
+	validates :distance, presence: { message: 'Please enter the distance of this route.' }
+	validates :terrain, length: { minimum: 2, message: 'Please select at least one of the terrain types' }
 
 	belongs_to :user
 
