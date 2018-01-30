fdf46383a654680962dbd38e85159d3c57a7159f
Davey Wright <Davey Wright davey.w.wright@gmail.com>
2018-01-30 14:54:11 +0000

    Add coursel to homepage

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,7 +1,9 @@
 class NotificationMailer < ApplicationMailer
 
+	# default from address is only a string so you can write anything you want ie. no-reply@shakalaka.com
 	default from: 'd.dawg.apps@gmail.com'
 
+	# take a comment parameter to further find the place and place_owner 
 	def comment_added(comment)
 		@place = comment.place
 		@place_owner = @place.user
