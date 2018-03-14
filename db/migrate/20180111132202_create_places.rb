08d07f474240bbb35443055889e2eabb6c9d10dc
Davey Wright <Davey Wright davey.w.wright@gmail.com>
2018-03-14 10:01:17 +0000

    Fix - places migration file

---
+++ db/migrate/20180111132202_create_places.rb
@@ -0,0 +1,16 @@
+class CreatePlaces < ActiveRecord::Migration[5.0]
+  def change
+    create_table :places do |t|
+     t.string   "title"
+     t.string   "difficulty"
+     t.string   "time"
+     t.float    "distance"
+     t.text     "description"
+     t.text     "directions"
+       t.text     "terrain",     default: [],              array: true
+     t.integer  "user_id"
+     t.index ["user_id"], name: "index_places_on_user_id", using: :btree
+      t.timestamps
+    end
+  end
+end
