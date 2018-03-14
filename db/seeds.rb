2f91b044e81cb7ee7d192bcd8498a5f296f7cb62
Davey Wright <Davey Wright davey.w.wright@gmail.com>
2018-03-14 12:08:02 +0000

    refactor production environment with sendgrid

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,5 +5,3 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
-# Place.create(title: 'route1', difficulty: 'easy stroll', )
